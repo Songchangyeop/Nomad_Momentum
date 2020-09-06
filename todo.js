3bb272f2d80bc607c1c764bd66caa743ec93dfa4
Songchangyeop <61750383+Songchangyeop@users.noreply.github.com>
2020-09-07 02:45:31 +0900

    JS: Make to Do list part 2

--- todo.js
+++ todo.js
@@ -4,27 +4,51 @@ const toDoForm = document.querySelector('.js-toDoForm'),
 
 const TODOS_LS = 'toDos';
 
+const toDos = [];
+
+// save localStorage
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+// Make tag and paint to Do list
 function paintToDo(text) {
   const li = document.createElement('li');
   const delBtn = document.createElement('button');
-  delBtn.innerText = 'X';
   const span = document.createElement('span');
+  const newId = toDos.length + 1;
+  delBtn.innerText = 'X';
   span.innerText = text;
   li.appendChild(span);
   li.appendChild(delBtn);
+  li.id = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    // toDolist Object
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
+
+// submit Input
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = toDoInput.value;
   paintToDo(currentValue);
   toDoInput.value = '';
 }
+
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
+  const loadedToDos = localStorage.getItem(TODOS_LS);
   if (toDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach((toDo) => paintToDo(toDo.text));
   }
 }
+
+// init
 function init() {
   loadToDos();
   toDoForm.addEventListener('submit', handleSubmit);
